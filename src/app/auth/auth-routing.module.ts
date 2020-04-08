14e5b2c371feb49544dd09e8f2b246b7ab48667d
Mahendran.M <mahendran.m@falcon.io>
2020-04-08 19:04:21 +0530

    Initial commit

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -18,6 +18,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModuledshmtd]
 })
 export class AuthRoutingModule {}
